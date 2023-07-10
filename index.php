ad80d57596445dab99982246dd7458a5335b7380
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-10 11:45:44 +0600

    45 connect to the db from php_s8

--- index.php
+++ index.php
@@ -1,63 +1,14 @@
-<?php 
-    $articles = [
-        [
-            "title" => "First post",
-             "content" => "This is the first of many posts!"
-        ],
-        [
-            "title" => "Another post",
-            "content" => "You must read this now, it's essential reading!"
-        ],
-        [
-            "title"=> "Read this!",
-            "content"=>"You must read this now, it's essential reading!"
-        ],
-        [
-            "title"=>"The latest news",
-            "content"=>"Here's the latest news, read it now"
-        ]
-        
-        ];
+<?
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "root";
+$db_pass = "";
 
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=My, initial-scale=1.0">
-    <title>My Blog</title>
-</head>
-<body>
-    <header>
-        <h1>My Blog</h1>
-    </header>
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
 
-    <main>
-        <ul>
-            <?php foreach($articles as $article):?>
-        <li>
-            <article>
-                <h2><?= $article['title']; ?></h2>
-                <p><?= $article['content']; ?></p>
-            </article>
-
-        </li>
-           <?php endforeach; ?>     
-
-    </ul>
-    </main>
-
-    <!-- 
-                t44-notes
-       sql queries
-       ----------
-      
-SELECT * FROM `article` WHERE title = 'Title for post one';
--
-SELECT * FROM article ORDER BY title DESC;
-
-     -->
-    
-</body>
-</html>
+echo "Connected successfully.";
