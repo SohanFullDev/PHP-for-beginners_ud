389327012de79ec76c0a1e084dcb9f4bbb5e53c4
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-10 15:05:18 +0600

    50 avoid sql injection-validate the id passed in from the query string_s9

--- article.php
+++ article.php
@@ -13,9 +13,12 @@ if (mysqli_connect_error()) {
     exit;
 }
 
+if(isset($_GET['id']) && is_numeric($_GET['id'])){
 $sql = "SELECT *
         FROM article
         WHERE id = " . $_GET['id'];
+
+        //var_dump($sql);
 
 $results = mysqli_query($conn, $sql);
 
@@ -23,6 +26,9 @@ if ($results === false) {
     echo mysqli_error($conn);
 } else {
     $article = mysqli_fetch_assoc($results);
+}
+}else {
+    $article = null;
 }
 
 ?>
